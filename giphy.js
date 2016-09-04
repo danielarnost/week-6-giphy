b182c2d895d6e61a573ccea602477a34fec1c869
danielarnost <danielarnost@utexaas.edu>
2016-09-04 10:10:14 -0500

    final?

--- giphy.js
+++ giphy.js
@@ -5,7 +5,7 @@
 
 
        
-
+//you can use ratings here after equals
         $.ajax({
                 url: queryURL,
                 method: 'GET'
@@ -13,30 +13,32 @@
             .done(function(response) {
                 var results = response.data;
                 console.log(results)
-
+//grabbing response from URL, creating the forloop for the above array of gifs iterate thogh gifs
                 for (var i = 0; i < results.length; i++) {
 
-
+//dont use one letter variables this new condition filters out stuff jeff added to the original get url query
+               //     if(result[i].rating != "r" && results[i].rating!="pg-13"){}
 
 
 
                     var gifDiv = $('<div class="item">')
-
+// storing div in gif div variable
                     var rating = results[i].rating;
 
                     var p = $('<p>').text("Rating: " + rating);
-
+//create image 
                     var animalImage = $('<img class = "animalImage" id = anIMG>');
                     //var animalImage = $('<img>');
                     animalImage.attr('src', results[i].images.fixed_height.url);
                     animalImage.attr('data-still', results[i].images.fixed_height_still.url);
                     animalImage.attr('data-animate', results[i].images.fixed_height.url);
                     animalImage.attr('data-state','animated');
-		            gifDiv.append(p)
+					//animalImage.attr('src', results[i].images.fixed_height.url);
+                    gifDiv.append(p)
                     gifDiv.append(animalImage)
                   
-                    $('#gifsAppearHere').prepend(gifDiv);
 
+                    $('#gifsAppearHere').prepend(gifDiv);
                 }
 
             });
@@ -65,8 +67,8 @@ $(this).attr('src', url);
 //add conditonal to make value of added button equal to data-animal?
 
 $('#addAnimal').on('click', function(){
-var addedButton = $('<button>').appendTo('#buttons');
-$(addedButton).attr('data-animal', $('#adder').val());
-$(addedButton).text($('#adder').val().trim());
+var addedButton = $('<button>').appendTo('#buttons')
+$(addedButton).attr('data-animal', $('#adder').val())
+$(addedButton).text($('#adder').val().trim())
 
-});
+})
